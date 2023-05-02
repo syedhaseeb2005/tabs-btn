c75fb613ad118a03b84b19e475f36624eb96f1d3
syedhaseeb2005 <haseebsyed444@gmail.com>
2023-05-02 22:04:27 +0500

    tabs-btn

---
+++ app.js
@@ -0,0 +1,55 @@
+// const btn = document.querySelector('.tabs-btn')
+const content = document.querySelector('.content')
+
+const history=()=>{
+    content.innerHTML = ` <h3>History</h3>
+    <p>  I'm baby wolf pickled schlitz try-hard normcore marfa man bun mumblecore vice pop-up XOXO lomo kombucha glossier bicycle rights. Umami kinfolk salvia jean shorts offal venmo. Knausgaard tilde try-hard, woke fixie banjo man bun. Small batch tumeric mustache tbh wayfarers 8-bit shaman chartreuse tacos. Viral direct trade hoodie ugh chambray, craft beer pork belly flannel tacos single-origin coffee art party migas plaid pop-up.</p>`
+};
+
+const vision=()=>{
+    content.innerHTML = ` <h3>Vision</h3>
+    <p>Man bun PBR&B keytar copper mug prism, hell of helvetica. Synth crucifix offal deep v hella biodiesel. Church-key listicle polaroid put a bird on it chillwave palo santo enamel pin, tattooed meggings franzen la croix cray. Retro yr aesthetic four loko tbh helvetica air plant, neutra palo santo tofu mumblecore. Hoodie bushwick pour-over jean shorts chartreuse shabby chic. Roof party hammock master cleanse pop-up truffaut, bicycle rights skateboard affogato readymade sustainable deep v live-edge schlitz narwhal.</p>
+    <div class="list">
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+    </div>`
+};
+
+const goal=()=>{
+    content.innerHTML = ` <h3>Goals</h3>
+    <p>Chambray authentic truffaut, kickstarter brunch taxidermy vape heirloom four dollar toast raclette shoreditch church-key. Poutine etsy tote bag, cred fingerstache leggings cornhole everyday carry blog gastropub. Brunch biodiesel sartorial mlkshk swag, mixtape hashtag marfa readymade direct trade man braid cold-pressed roof party. Small batch adaptogen coloring book heirloom. Letterpress food truck hammock literally hell of wolf beard adaptogen everyday carry. Dreamcatcher pitchfork yuccie, banh mi salvia venmo photo booth quinoa chicharrones.</p>`
+};
+// const element = []
+
+// const content = [
+//     {
+//         id: 1,
+//         title: 'Vision',
+//         para:'Man bun PBR&B keytar copper mug prism, hell of helvetica. Synth crucifix offal deep v hella biodiesel. Church-key listicle polaroid put a bird on it chillwave palo santo enamel pin, tattooed meggings franzen la croix cray. Retro yr aesthetic four loko tbh helvetica air plant, neutra palo santo tofu mumblecore. Hoodie bushwick pour-over jean shorts chartreuse shabby chic. Roof party hammock master cleanse pop-up truffaut, bicycle rights skateboard affogato readymade sustainable deep v live-edge schlitz narwhal.'
+//     },
+//     {
+//         id:2,
+//         title:"Goal",
+//         para:'Chambray authentic truffaut, kickstarter brunch taxidermy vape heirloom four dollar toast raclette shoreditch church-key. Poutine etsy tote bag, cred fingerstache leggings cornhole everyday carry blog gastropub. Brunch biodiesel sartorial mlkshk swag, mixtape hashtag marfa readymade direct trade man braid cold-pressed roof party. Small batch adaptogen coloring book heirloom. Letterpress food truck hammock literally hell of wolf beard adaptogen everyday carry. Dreamcatcher pitchfork yuccie, banh mi salvia venmo photo booth quinoa chicharrones.'
+//     }
+// ]
+
+// const filter = content.map((item)=>{
+//     return `<header>
+//     <div>
+//     <p>
+//     <h3>${item.title}</h3>
+//     ${item.para}
+//     </p>
+//     </div>
+//     </header`
+// });
+// visionbtn.innerHTML = filter.join("")
+
+
+
+
+
+
+
